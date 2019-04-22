d0e7c192650148530fcde9a2fd5c77c2f8ca3aee
debasis123 <debasis.mandal@gmail.com>
2019-04-22 15:30:35 -0400

    dead code removal

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -17,22 +17,6 @@ KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
 
-// void
-// KalmanFilter::Init(VectorXd& x_in,
-//                    MatrixXd& P_in,
-//                    MatrixXd& F_in,
-//                    MatrixXd& H_in,
-//                    MatrixXd& R_in,
-//                    MatrixXd& Q_in)
-// {
-//   x_ = x_in;
-//   P_ = P_in;
-//   F_ = F_in;
-//   H_ = H_in;
-//   R_ = R_in;
-//   Q_ = Q_in;
-// }
-
 void KalmanFilter::Predict() {
   // predict the state
   x_          = F_*x_;
